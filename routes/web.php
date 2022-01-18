4bd587c8993ce3d94527c9d5a5dfa422eb526274
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-01-18 08:17:22 -0800

    traverse to a new page after log in

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+    return view('admin-panel/dashboard');
+})->name('admin-panel.dashboard');
